9271cd626639bce141b91937f73599162eb1b2cb
Nick Frazier <nrflaw@gmail.com>
2017-07-27 09:02:46 -0400

    added localstorage support

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,12 +1,5 @@
+/*global io */
 var socket = io();
-
-var messageNode = document.getElementById('message');
-
-socket.on('message', function(msg) {
-  messageNode.innerHTML = msg;
-  messageNode.textFitHeight();
-  // messageNode.textFit(0.5, { min: 50, max: 150 });
-});
 
 Element.prototype.textFitHeight = function(_options) {
   const options = _options || {
@@ -14,7 +7,7 @@ Element.prototype.textFitHeight = function(_options) {
     max: 100
   };
 
-  const resizer = () => {
+  const shrinker = () => {
     this.style.fontSize = ''; // reset to default CSS property
 
     let currentSize = options.max;
@@ -26,9 +19,55 @@ Element.prototype.textFitHeight = function(_options) {
     }
   };
 
-  resizer();
+  shrinker();
 
   // TODO: debounce
-  window.addEventListener('resize', resizer);
-  window.addEventListener('orientationchange', resizer);
+  window.addEventListener('resize', shrinker);
+  window.addEventListener('orientationchange', shrinker);
+};
+
+function storageAvailable(type) {
+  // localStorage detection; from developer.mozilla.org
+  try {
+    var storage = window[type],
+      x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0
+    );
+  }
+}
+
+// main
+window.onload = function() {
+  let localstorage = null;
+  const messageNode = document.getElementById('message');
+
+  if (storageAvailable('localStorage')) {
+    localstorage = window.localStorage;
+    if (localstorage.getItem('msg')) {
+      messageNode.innerHTML = localstorage.getItem('msg');
+      messageNode.textFitHeight();
+    }
+  }
+
+  socket.on('message', function(msg) {
+    messageNode.innerHTML = msg;
+    messageNode.textFitHeight();
+    if (localstorage) localstorage.setItem('msg', msg);
+  });
 };
